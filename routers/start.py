4b722038836d0a4a80212e8cba384cfb50a0abf4
stepanshustov <step.shustov@mail.ru>
2026-09-19 22:27:15 +0400

    feat: start message

--- routers/start.py
+++ routers/start.py
@@ -3,18 +3,18 @@ from maxapi.enums.format import Format
 from maxapi.filters.command import CommandStart, Command
 from maxapi.types import BotStarted, MessageCreated
 
-
 from texts import HELLO_TEXT
-
+from keyboards import get_main_keyboard
 
 router = Router(router_id="start")
+
 
 @router.bot_started()
 async def on_bot_started(event: BotStarted):
     """Event handler for bot started, before the command /start"""
     pass
 
-@router.message_created(CommandStart)
-async def start_command(event: MessageCreated):
-    await event.message.answer(HELLO_TEXT.text)
 
+@router.message_created(Command('start'))
+async def start_command(event: MessageCreated):
+    await event.message.answer(HELLO_TEXT.text, attachments=[await get_main_keyboard()])
