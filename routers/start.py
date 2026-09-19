bcff31c4b7d3b0fc3d869d7f79a7622355f459a2
stepanshustov <step.shustov@mail.ru>
2026-09-19 20:37:00 +0400

    init start routers

---
+++ routers/start.py
@@ -0,0 +1,20 @@
+from maxapi import F, Router
+from maxapi.enums.format import Format
+from maxapi.filters.command import CommandStart
+from maxapi.types import BotStarted, MessageCreated
+
+
+from texts import HELLO_TEXT
+
+
+router = Router(router_id="start")
+
+@router.bot_started()
+async def on_bot_started(event: BotStarted):
+    """События создания чата с ботом, до команды /start"""
+    pass
+
+@router.message_created(CommandStart)
+async def start_command(event: MessageCreated):
+    await event.message.answer(HELLO_TEXT.text)
+
