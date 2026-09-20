1b70c89091d33c0260de46b8a07431dc755a7341
stepanshustov <step.shustov@mail.ru>
2026-09-20 13:59:55 +0400

    style: добавлена правильная аннотация

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,9 +1,10 @@
 from maxapi.types.attachments.buttons import CallbackButton
 from maxapi.utils.inline_keyboard import InlineKeyboardBuilder
 from callbacks import MenuPayload
+from maxapi.types.attachments.buttons.attachment_button import AttachmentButton
 
 
-async def get_main_keyboard():
+async def get_main_keyboard() -> AttachmentButton:
     keyboard = InlineKeyboardBuilder()
     keyboard.row(
         CallbackButton(text="демо оставить заявку", payload=MenuPayload(action="form").pack())
